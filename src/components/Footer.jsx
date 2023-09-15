e6f301eb8347361b3af3e3c1b4d4db4d388fa2fa
Haseenarh <hassanatbello93@gmail.com>
2023-09-15 16:31:09 +0100

    Recreated moviebox

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import facebook from "../assets/footer/facebook.svg";
+import instagram from "../assets/footer/instagram.svg";
+import twitter from "../assets/footer/twitter.svg"
+import youtube from "../assets/footer/youtube.svg";
+
+
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-links">
+        <img src={facebook} alt="" />
+        <img src={instagram} alt="" />
+        <img src={youtube} alt="" />
+      </div>
+      <div className="footer-info">
+        <p>Conditions of Use</p>
+        <p>Privacy & Policy</p>
+        <p>Press Room</p>
+      </div>
+      <div className="copyright">
+        <p>© 2023 MovieBox by Hassanat Bello </p>
+      </div>
+    </div>
+  );
+};
